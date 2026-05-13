8a93ff95ed049d345f68859c7502a64bc84f463b
eloyarnel <eloyaliceasanchez@gmail.com>
2026-05-13 12:56:40 -0400

    Replace element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,10 +3,9 @@ def replace_in_list(my_list, idx, element):
     # Check if idx is negative
     if idx < 0:
         return my_list
-    # Check if idx is out of range (greater than or equal to the length of the list)
+    # Check if idx is out of range
     if idx >= len(my_list):
         return my_list
-    # If idx is valid, replace the element at that index
+    # Replace the element
     my_list[idx] = element
-    # Return the modified list
     return my_list
