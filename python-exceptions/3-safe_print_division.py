703b94374756fed7592c8aa3ab35acadaac965a2
eloyarnel <eloyaliceasanchez@gmail.com>
2026-05-15 12:29:00 -0400

    Integers division with debug

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Divide two integers and print the result."""
+
+    result = None
+
+    try:
+        result = a / b
+        return result
+
+    except Exception:
+        return None
+
+    finally:
+        print("Inside result: {}".format(result))
