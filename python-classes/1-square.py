24c63325cb37ec6bebdd3e29b113b8fde27dc4ae
eloyarnel <eloyaliceasanchez@gmail.com>
2026-05-19 12:03:02 -0400

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
