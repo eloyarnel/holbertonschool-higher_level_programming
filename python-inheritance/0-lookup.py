7f90b25ae596b371fa71bca2b0ec601744b9fba2
eloyarnel <eloyaliceasanchez@gmail.com>
2026-05-26 15:14:50 -0400

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module to list attributes and methods of an object."""
 
+
 def lookup(obj):
     """Return list of available attributes and methods of an object."""
     return dir(obj)
