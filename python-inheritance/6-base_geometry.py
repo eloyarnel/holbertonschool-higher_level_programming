6458ccb752223bfea94ca44db20026476599c669
eloyarnel <eloyaliceasanchez@gmail.com>
2026-05-26 15:32:09 -0400

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a BaseGeometry class with area method."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Calculate area of geometry."""
+        raise Exception("area() is not implemented")
