cb9f94c7d9585e76ddbc05018298325dcdb75c70
eloyarnel <eloyaliceasanchez@gmail.com>
2026-06-02 15:29:12 -0400

    Write a function that reads a text file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module that defines a function to read a text file
+and print its contents to stdout.
+"""
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints its contents to stdout.
+
+    Args:
+        filename (str): name of the file to read
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
