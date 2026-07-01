3b4edc69ffcbbcb3dd0c452abf083e5cc4942963
eloyarnel <eloyaliceasanchez@gmail.com>
2026-07-01 16:28:53 -0400

    Python Object Relational code exercise

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Lists all states starting with 'N' from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+    cursor.execute(query)
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
